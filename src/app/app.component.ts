d3ca35381ab76054b4d754dbc61f156c40be05e2
Sayan Samanta <sayansamanta@Sayans-Mac-mini.local>
2024-12-06 12:54:33 +0530

    06-12-2024: profile theme added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'profile-app';
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http
+      .request('GET', `${''}${'/api/quotes'}`, {
+        responseType: 'json',
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
